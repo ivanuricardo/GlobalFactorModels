15a705f2b8e096afc9f3ae47f36fa8decc1b741b
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-04-03 23:16:41 +0200

    added preprocessing steps

---
+++ R/preprocessing.R
@@ -0,0 +1,53 @@
+###### Preprocessing for Models
+
+library(readxl)
+library(tidyverse)
+library(reshape2)
+set.seed(20230322)
+
+
+######## Tensor Factor Model Preprocessing
+
+pathname <- "/Users/Ivan/Desktop/gvar_database/country_data.xls"
+sheet_names <- excel_sheets(pathname)
+data <- read_excel(pathname, sheet = 1)
+gvar_data <- data%>%mutate(country = sheet_names[1])
+
+for (sheet in (2:(length(sheet_names)-1))) {
+  appending_data <- read_excel(pathname, sheet = sheet)
+  appending_data <- appending_data%>%mutate(country = sheet_names[sheet])
+  new_data <- plyr::rbind.fill(gvar_data, appending_data)
+  gvar_data <- new_data
+}
+
+# remove unnecessary columns and columns with NA. Additionally remove NL from ROW
+sa_gvar1 <- subset(gvar_data, country != "SAUDI ARABIA")
+sa_gvar2 <- subset(sa_gvar1, country != "NETHERLANDS")
+sa_gvar2[, c("date", "country", "eps", "poil", "pmetal", "pmat")] <- list(NULL)
+updated_gvar <- sa_gvar2[ , colSums(is.na(sa_gvar2))==0]
+
+# Same for NL variables
+nl_variables <- subset(gvar_data, country == "NETHERLANDS")
+nl_variables[, c("date", "country", "eps")] <- list(NULL)
+
+# Transform NL to stationarity and remove first two observations
+stat_nl_var <- order_integration(nl_variables)
+stat_nl <- stat_nl_var$diff_data[-1:-2,]
+
+# create 3-dimensional tensor of ROW
+levels_tensor_data <- updated_gvar %>% 
+  as.matrix() %>% 
+  array(dim = c(163, 31, 5)) 
+
+dim(levels_tensor_data)
+
+# Flatten to convert all series to stationary
+perm_tensor_data <- aperm(levels_tensor_data, c(1,3,2))
+mat_data <- tensorFun::unfold(perm_tensor_data, 1)
+
+# Perform Unit Root tests and difference the series appropriately
+# I lose the first two observations
+
+stat_tensor_data <- order_integration(mat_data)
+final_stat_data <- stat_tensor_data$diff_data[-1:-2,]
+final_stat_tensor <- aperm(array(final_stat_data, dim = c(161, 5, 31)), c(1,3,2))
