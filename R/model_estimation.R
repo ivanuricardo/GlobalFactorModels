8eba136dd0020da4eb443f96a14950c3c924b33d
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-04-04 12:50:30 +0200

    double check everything is working

--- R/model_estimation.R
+++ R/model_estimation.R
@@ -24,6 +24,7 @@ var_forecast_residuals <- t(matrix((first_forecast_var - var_test[1,])^2))
 
 num_forecasts <- 49
 
+# Ignore the error at the end
 for (sim in 2:num_forecasts) {
   sim_lasso_var <- sparseVAR(scale(normal_var_data[(1+sim):(112+sim),]), p = 1,
                              VARpen = "L1",
@@ -31,7 +32,6 @@ for (sim in 2:num_forecasts) {
   sim_forecast_var <- directforecast(sim_lasso_var)
   var_forecast_residuals <- rbind(var_forecast_residuals,
                                   (sim_forecast_var - var_test[sim,])^2)
-  print(sim)
 }
 
 # Plot the residuals
@@ -69,13 +69,13 @@ favar_forecast_residuals <- t(matrix((first_forecast_favar - favar_test[1,])^2))
 
 num_forecasts <- 49
 
+# ignore the last error...
 for (sim in 2:num_forecasts) {
   sim_favar <- VAR(favar_data[(1+sim):(112+sim),], p = 2, type = 'none')
   sim_prediction_favar <- predict(sim_favar, n.ahead = 1)
   sim_forecast_favar <- sapply(sim_prediction_favar$fcst, `[`, 1)
   favar_forecast_residuals <- rbind(favar_forecast_residuals,
                                     (sim_forecast_favar - favar_test[sim,])^2)
-  print(sim)
 }
 
 par(mfrow = c(3, 3), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
@@ -92,7 +92,6 @@ favar_res_plot5 <- plot(favar_forecast_residuals[,12], type = 'l', main = "Mater
                         xlab = "Time", ylab = "Residual")
 favar_res_plot6 <- plot(favar_forecast_residuals[,13], type = 'l', main = "Metal Prices",
                         xlab = "Time", ylab = "Residual")
-favar_res_plot_title <- title("Squared Residual Plots for FAVAR")
 
 # GFAVAR
 gfavar_data <- readRDS("data/GFAVARdata.rds")
@@ -111,13 +110,12 @@ gfavar_forecast_residuals <- data.frame(t(matrix((first_forecast_gfavar -
 num_forecasts <- 49
 
 for (sim in 2:num_forecasts) {
-  sim_gfavar <- VAR(full_data_matrix[(1+sim):(112+sim),], p = 3, type = 'none')
+  sim_gfavar <- VAR(gfavar_data[(1+sim):(112+sim),], p = 3, type = 'none')
   sim_prediction_gfavar <- predict(sim_gfavar, n.ahead = 1)
   sim_forecast_gfavar <- sapply(sim_prediction_gfavar$fcst, `[`, 1)
   gfavar_res <- data.frame((sim_forecast_gfavar - gfavar_test[sim,])^2)
   colnames(gfavar_res) <- colnames(gfavar_forecast_residuals)
   gfavar_forecast_residuals <- rbind(gfavar_forecast_residuals, gfavar_res)
-  print(sim)
 }
 
 par(mfrow = c(3, 3), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
