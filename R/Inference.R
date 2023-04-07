bc0bf60725784c329be0cf2757ee1fb0d56ff91d
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-04-07 16:13:24 +0200

    add irf inference

--- R/Inference.R
+++ R/Inference.R
@@ -1,5 +1,7 @@
 library(ggplot2)
-library(gridExtra)
+library(ggpubr)
+library(desla)
+library(HDGCvar)
 set.seed(20230322)
 
 ####### Matrix DFM of Wang
@@ -49,31 +51,150 @@ df6 <- data.frame(
 countryecon11 <- ggplot(df1, aes(x = series_names, y = r_squared)) +
     geom_bar(stat = "identity", fill = "blue") +
     labs(x = "Series Names", y = "R-Squared") +
-    ggtitle("First Country Factor, First Econ Factor")
+    ggtitle("1st Country, 1st Econ")
 
 countryecon21 <- ggplot(df2, aes(x = series_names, y = r_squared)) +
   geom_bar(stat = "identity", fill = "blue") +
   labs(x = "Series Names", y = "R-Squared") +
-  ggtitle("Second Country Factor, First Econ Factor")
+  ggtitle("2nd Country, 1st Econ")
 
 countryecon12 <- ggplot(df3, aes(x = series_names, y = r_squared)) +
   geom_bar(stat = "identity", fill = "blue") +
   labs(x = "Series Names", y = "R-Squared") +
-  ggtitle("First Country Factor, Second Econ Factor")
+  ggtitle("1st Country, 2nd Econ")
 
 countryecon22 <- ggplot(df4, aes(x = series_names, y = r_squared)) +
   geom_bar(stat = "identity", fill = "blue") +
   labs(x = "Series Names", y = "R-Squared") +
-  ggtitle("Second Country Factor, Second Econ Factor")
+  ggtitle("2nd Country, 2nd Econ")
 
 countryecon13 <- ggplot(df5, aes(x = series_names, y = r_squared)) +
   geom_bar(stat = "identity", fill = "blue") +
   labs(x = "Series Names", y = "R-Squared") +
-  ggtitle("First Country Factor, Third Econ Factor")
+  ggtitle("1st Country, 3rd Econ")
 
-countryecon32 <- ggplot(df6, aes(x = series_names, y = r_squared)) +
+countryecon23 <- ggplot(df6, aes(x = series_names, y = r_squared)) +
   geom_bar(stat = "identity", fill = "blue") +
   labs(x = "Series Names", y = "R-Squared") +
-  ggtitle("Second Country Factor, Third Econ Factor")
+  ggtitle("2nd Country, 3rd Econ")
+
+figure1 <- ggarrange(countryecon11, countryecon12, countryecon13,
+                    countryecon21, countryecon22, countryecon23,
+                    ncol = 3, nrow = 2)
+figure
 
 
+####### Local Projection Inference
+
+# First, reorder the data such that fast moving variables are on the right
+# and slow moving variables are on the left.
+new_order <- c(15:29, 1, 6, 2, 7, 11, 12, 13, 14, 10, 4, 3, 8, 5, 9)
+reordered_matdfm <- matrixdfm_data[,new_order]
+
+
+### IR to GDP
+
+shock_idx <- 28
+response_idx <- 16
+
+shock_var <- reordered_matdfm[,shock_idx]
+response_var <- reordered_matdfm[,response_idx]
+
+updated_matdfm <- reordered_matdfm[, -c(shock_idx, response_idx)]
+slow_vars <- updated_matdfm[,1:13]
+fast_vars <- updated_matdfm[,14:27]
+
+# Local Projections
+LP_ry <- HDLP(x = shock_var, y = response_var, r = slow_vars, q = fast_vars,
+              hmax = 20, lags = 10)
+graph_yr <- plot.hdlp(LP_ry, response = "GDP", impulse = "IR")
+
+### IR to Factor 11
+
+shock_idx <- 28
+response_idx <- 11
+
+shock_var <- reordered_matdfm[,shock_idx]
+response_var <- reordered_matdfm[,response_idx]
+
+updated_matdfm <- reordered_matdfm[, -c(shock_idx, response_idx)]
+slow_vars <- updated_matdfm[,1:13]
+fast_vars <- updated_matdfm[,14:27]
+
+# Local Projections
+LP_r11 <- HDLP(x = shock_var, y = response_var, r = slow_vars, q = fast_vars,
+              hmax = 20, lags = 10)
+graph_yf11 <- plot.hdlp(LP_r11, response = "FAC11", impulse = "IR")
+
+### IR to Dps
+
+shock_idx <- 28
+response_idx <- 19
+
+shock_var <- reordered_matdfm[,shock_idx]
+response_var <- reordered_matdfm[,response_idx]
+
+updated_matdfm <- reordered_matdfm[, -c(shock_idx, response_idx)]
+slow_vars <- updated_matdfm[,1:13]
+fast_vars <- updated_matdfm[,14:27]
+
+# Local Projections
+LP_dps <- HDLP(x = shock_var, y = response_var, r = slow_vars, q = fast_vars,
+              hmax = 20, lags = 10)
+graph_ydps <- plot.hdlp(LP_dps, response = "Dps", impulse = "IR")
+
+### IR to PMAT
+
+shock_idx <- 28
+response_idx <- 21
+
+shock_var <- reordered_matdfm[,shock_idx]
+response_var <- reordered_matdfm[,response_idx]
+
+updated_matdfm <- reordered_matdfm[, -c(shock_idx, response_idx)]
+slow_vars <- updated_matdfm[,1:13]
+fast_vars <- updated_matdfm[,14:27]
+
+# Local Projections
+LP_rpmat <- HDLP(x = shock_var, y = response_var, r = slow_vars, q = fast_vars,
+              hmax = 20, lags = 10)
+graph_ypmat <- plot.hdlp(LP_rpmat, response = "PMAT", impulse = "IR")
+
+### IR to PMETAL
+
+shock_idx <- 28
+response_idx <- 22
+
+shock_var <- reordered_matdfm[,shock_idx]
+response_var <- reordered_matdfm[,response_idx]
+
+updated_matdfm <- reordered_matdfm[, -c(shock_idx, response_idx)]
+slow_vars <- updated_matdfm[,1:13]
+fast_vars <- updated_matdfm[,14:27]
+
+# Local Projections
+LP_rpmetal <- HDLP(x = shock_var, y = response_var, r = slow_vars, q = fast_vars,
+              hmax = 20, lags = 10)
+graph_ypmetal <- plot.hdlp(LP_rpmetal, response = "PMET", impulse = "IR")
+
+### EX to GDP
+
+shock_idx <- 25
+response_idx <- 16
+
+shock_var <- reordered_matdfm[,shock_idx]
+response_var <- reordered_matdfm[,response_idx]
+
+updated_matdfm <- reordered_matdfm[, -c(shock_idx, response_idx)]
+slow_vars <- updated_matdfm[,1:13]
+fast_vars <- updated_matdfm[,14:27]
+
+# Local Projections
+LP_exy <- HDLP(x = shock_var, y = response_var, r = slow_vars, q = fast_vars,
+              hmax = 20, lags = 10)
+graph_exy <- plot.hdlp(LP_exy, response = "GDP", impulse = "EXR")
+
+figure2 <- ggarrange(graph_yr, graph_yf11, graph_ydps,
+                     graph_ypmat, graph_ypmetal, graph_exy,
+                     ncol = 3, nrow = 2)
+figure2
